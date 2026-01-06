6ff70e1c3684d14764a2a751504a0a3848eac61c
shlomoPearl <shlomoper94@gmail.com>
2026-01-06 11:58:35 +0200

    add 20 connection in loop

--- test.sh
+++ test.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo '{EVENT:test,TIME:2024-12-30}' | nc localhost 7777
+echo '{"EVENT":"test","TIME":"2024-12-30"}' | nc localhost 7777
+for i in {1..20}; do
+    echo '{"EVENT":"test_'$i'","TIME":"2024-12-30"}' | nc localhost 7777
+done
