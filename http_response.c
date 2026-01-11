36f077b57018f54d55d914ba7e8a4dcc50483157
shlomoPearl <shlomoper94@gmail.com>
2026-01-11 23:53:28 +0200

    add response header to crete string and send it back

---
+++ http_response.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "http_response.h"
+
+#define K 0
+#define V 1
+
+void response_to_s(char* response_s, HTTPResponse http_res, size_t size){
+    const char* delimiter = "\r\n";
+    int count = 0;
+
+    // response line
+    count += snprintf(response_s, 4096-count, "%s", http_res.version);
+    count += snprintf(response_s + count, 4096-count," %d",http_res.status_code);
+    count += snprintf(response_s + count, 4096-count, " %s%s",http_res.status_text , delimiter);
+    
+    // headers
+    for (int i=0; i < http_res.header_count; i++){
+        count += snprintf(response_s + count, 4096-count, "%s: %s%s",http_res.headers[i][K],http_res.headers[i][V], delimiter);
+    }
+    // blank line
+    count += snprintf(response_s + count, 4096-count, "%s",delimiter);
+    // body
+    count += snprintf(response_s + count, 4096-count, "%s%s",http_res.body , delimiter);
+}
---
+++ http_response.h
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+typedef struct {
+    char version[16];       
+    int status_code;        
+    char status_text[16];        
+    char headers[20][2][256]; // Array of [key][value]
+    int header_count;
+    char body[2048];        
+    int body_length;
+} HTTPResponse;
+
+void response_to_s(char* ,HTTPResponse, size_t); 
