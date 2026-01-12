600cb0dbf7a43a2dca198aedff258bc54ffe625b
shlomoPearl <shlomoper94@gmail.com>
2026-01-12 14:03:36 +0200

    add test withou json

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-for i in {1..2}; do
+for i in {1..5}; do
     curl -X POST http://localhost:7777   -H "Content-Type: application/json"   -d '{"EVENT":"test_'$i'","TIME":"2024-12-30"}'
 done
+
+curl -v http://localhost:7777/TEST
