b975225dda0512e6518ded4edd4b9b4f87a9c97d
shlomoPearl <shlomoper94@gmail.com>
2026-01-20 14:21:28 +0200

    simple warp function to basic pdf file operation - add page, write, set font and size, save and free pdf obj

--- pdf.c
+++ pdf.c
@@ -1,29 +1,29 @@
 #include "pdf.h"
 
-HPDF_Page get_pdf_report(){
-    HPDF_Doc pdf = HPDF_New(NULL, NULL);
-    if (!pdf) {
-        printf("Error: Cannot create PDF\n");
-        return 1;
-    }
-    
-    HPDF_Page page = HPDF_AddPage(pdf);
+HPDF_Page add_page(HPDF_Doc* pdf){
+    HPDF_Page page = HPDF_AddPage(*pdf);
     HPDF_Page_SetSize(page, HPDF_PAGE_SIZE_A4, HPDF_PAGE_PORTRAIT);
-    
-    HPDF_Font font = HPDF_GetFont(pdf, "Helvetica-Bold", NULL);
-    HPDF_Page_SetFontAndSize(page, font, 24);
-    test_pdf(page, "HI THERE THIS IS a TEST PDF",0);
-    font = HPDF_GetFont(pdf, "Helvetica", NULL);
-    HPDF_Page_SetFontAndSize(page, font, 12);
-    
-}
-void write_event(HPDF_Page page, const char* txt){
+    return page;
+}   
 
-}
-void save(HPDF_Doc pdf, const char* file_name){
-    HPDF_SaveToFile(pdf, file_name);
-}
-void free_pdf(HPDF_Doc pdf){
-    HPDF_Free(pdf);
+void set_font_and_size(HPDF_Doc* pdf, HPDF_Font* font, HPDF_Page* page, const char* font_name, int font_size){
+    *font = HPDF_GetFont(*pdf, font_name, NULL);
+    HPDF_Page_SetFontAndSize(*page, *font, font_size);
 }
 
+void write_to_pdf(HPDF_Doc* pdf,HPDF_Page* page, const char* txt, int w, int h){
+    if (HPDF_Page_GetHeight(*page) - h < 50){
+        *page = add_page(pdf);
+    }
+    HPDF_Page_BeginText(*page);
+    HPDF_Page_TextOut(*page, w , h, txt);
+    HPDF_Page_EndText(*page);
+}
+
+void save_pdf(HPDF_Doc* pdf, const char* file_name){
+    HPDF_SaveToFile(*pdf, file_name);
+}
+
+void free_pdf(HPDF_Doc* pdf){
+    HPDF_Free(*pdf);
+}
--- pdf.h
+++ pdf.h
@@ -1,24 +1,8 @@
 #include <time.h>
 #include <hpdf.h>
 
-#define TITLE "EVENT REPORT"
-#define T_FONT "Helvetica-Bold"
-#define T_SIZE 24
-#define B_FONT "Helvetica"
-#define B_SIZE 14
-
-typedef struct {
-    char* t_font;
-    char* t_size;
-    char* b_font;
-    char* b_size;
-    char* title;
-    char* dt_gen;
-
-} Report_PDF;
-
-HPDF_Page get_pdf_report();
-void write_event(HPDF_Page, const char*);
-void save(HPDF_Doc, const char*);
-void free_pdf(HPDF_Doc);
-
+HPDF_Page add_page(HPDF_Doc*);
+void set_font_and_size(HPDF_Doc*, HPDF_Font*,HPDF_Page*, const char*, int);
+void write_to_pdf(HPDF_Doc*,HPDF_Page*, const char*, int, int);
+void save_pdf(HPDF_Doc*, const char*);
+void free_pdf(HPDF_Doc*);
