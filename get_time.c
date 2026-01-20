e993c52e53352cb89ae9077de1626ea5b2a5390a
shlomoPearl <shlomoper94@gmail.com>
2026-01-20 15:30:33 +0200

    add get time_t and check if pass log/pdf report time

--- get_time.c
+++ get_time.c
@@ -1,11 +1,21 @@
 #include "get_time.h"
 
-char* get_current_time_str(char* buffer, size_t size) { 
+void get_current_time_str(char* buffer, size_t size) { 
     time_t now;
     struct tm *local_time;    
     time(&now);
     local_time = localtime(&now);
     strftime(buffer, size, "%Y-%m-%d %H:%M:%S", local_time);
-    return buffer;
 }
 
+void get_current_time(time_t* t){
+    time(t);
+}
+
+bool time_pass(int hours, time_t* start, time_t* end){
+    double diff = difftime(*end, *start);
+    if (diff >= hours * 3600){
+        return true;
+    }    
+    return false;
+}
--- get_time.h
+++ get_time.h
@@ -1,4 +1,7 @@
 #include <time.h>
+#include <stdbool.h>
 #include <stdlib.h>
 
-char* get_current_time_str(char*, size_t);
+void get_current_time_str(char*, size_t);
+void get_current_time(time_t*);
+bool time_pass(int, time_t*, time_t*);
