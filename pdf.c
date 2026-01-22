3ef81c528590fb383e3b019e3c787c442df8531d
shlomoPearl <shlomoper94@gmail.com>
2026-01-23 00:50:19 +0200

    add pdf struct

--- pdf.c
+++ pdf.c
@@ -1,29 +1,34 @@
 #include "pdf.h"
 
-HPDF_Page add_page(HPDF_Doc* pdf){
-    HPDF_Page page = HPDF_AddPage(*pdf);
-    HPDF_Page_SetSize(page, HPDF_PAGE_SIZE_A4, HPDF_PAGE_PORTRAIT);
-    return page;
+void add_page(PDF* pdf){
+    pdf->page = HPDF_AddPage(pdf->doc);
+    HPDF_Page_SetSize(pdf->page, HPDF_PAGE_SIZE_A4, HPDF_PAGE_PORTRAIT);
+    pdf->y_pos = HPDF_Page_GetHeight(pdf->page);
+    pdf->width = HPDF_Page_GetWidth(pdf->page);
 }   
 
-void set_font_and_size(HPDF_Doc* pdf, HPDF_Font* font, HPDF_Page* page, const char* font_name, int font_size){
-    *font = HPDF_GetFont(*pdf, font_name, NULL);
-    HPDF_Page_SetFontAndSize(*page, *font, font_size);
+void set_font_and_size(PDF* pdf, const char* font_name, int font_size){
+    pdf->font = HPDF_GetFont(pdf->doc, font_name, NULL);
+    HPDF_Page_SetFontAndSize(pdf->page, pdf->font, font_size);
 }
 
-void write_to_pdf(HPDF_Doc* pdf,HPDF_Page* page, const char* txt, int w, int h){
-    if (HPDF_Page_GetHeight(*page) - h < 50){
-        *page = add_page(pdf);
+void write_to_pdf(PDF* pdf, const char* txt, int x){
+    if (pdf->y_pos < 50){
+        const char* font_name = HPDF_Font_GetFontName(pdf->font);
+        int font_size = HPDF_Page_GetCurrentFontSize(pdf->page);
+        add_page(pdf);
+        set_font_and_size(pdf, font_name, font_size);
+        pdf->y_pos-= pdf->border;
     }
-    HPDF_Page_BeginText(*page);
-    HPDF_Page_TextOut(*page, w , h, txt);
-    HPDF_Page_EndText(*page);
+    HPDF_Page_BeginText(pdf->page);
+    HPDF_Page_TextOut(pdf->page, x , pdf->y_pos, txt);
+    HPDF_Page_EndText(pdf->page);
 }
 
-void save_pdf(HPDF_Doc* pdf, const char* file_name){
-    HPDF_SaveToFile(*pdf, file_name);
+void save_pdf(PDF* pdf, const char* file_name){
+    HPDF_SaveToFile(pdf->doc, file_name);
 }
 
-void free_pdf(HPDF_Doc* pdf){
-    HPDF_Free(*pdf);
+void free_pdf(PDF* pdf){
+    HPDF_Free(pdf->doc);
 }
--- pdf.h
+++ pdf.h
@@ -1,8 +1,17 @@
 #include <time.h>
 #include <hpdf.h>
 
-HPDF_Page add_page(HPDF_Doc*);
-void set_font_and_size(HPDF_Doc*, HPDF_Font*,HPDF_Page*, const char*, int);
-void write_to_pdf(HPDF_Doc*,HPDF_Page*, const char*, int, int);
-void save_pdf(HPDF_Doc*, const char*);
-void free_pdf(HPDF_Doc*);
+typedef struct PDF{
+    HPDF_Doc doc;
+    HPDF_Font font;
+    HPDF_Page page;
+    int y_pos;
+    int width;
+    int border;
+} PDF;
+
+void add_page(PDF*);
+void set_font_and_size(PDF*, const char*, int);
+void write_to_pdf(PDF*, const char*, int);
+void save_pdf(PDF*, const char*);
+void free_pdf(PDF*);
