f5791d45872c38abc7d18ccd994858a6740a2f8d
shlomoPearl <shlomoper94@gmail.com>
2026-01-25 23:36:17 +0200

    add init to pdf and struct pdf

--- pdf.c
+++ pdf.c
@@ -25,6 +25,30 @@ void write_to_pdf(PDF* pdf, const char* txt, int x){
     HPDF_Page_EndText(pdf->page);
 }
 
+void init_pdf(PDF* pdf){
+    pdf->doc = HPDF_New(NULL, NULL);
+    if (!pdf->doc) {
+        printf("Error: Cannot create PDF object\n");
+        return;
+    }
+    add_page(pdf);
+    pdf->border = BORDER;
+    set_font_and_size(pdf, T_FONT, T_SIZE);
+    float tw = HPDF_Page_TextWidth (pdf->page, TITLE);
+    pdf->y_pos -= pdf->border;
+    write_to_pdf(pdf, TITLE, (pdf->width - tw) / 2);
+    
+    char sub_titel_buffer[TIME_LEN];
+    strncpy(sub_titel_buffer, SUB_TITLE, strlen(SUB_TITLE) + 1);
+    get_current_time_str(sub_titel_buffer + strlen(SUB_TITLE), TIME_LEN - strlen(SUB_TITLE));
+    set_font_and_size(pdf, ST_FONT, ST_SIZE);
+    tw = HPDF_Page_TextWidth (pdf->page, sub_titel_buffer);
+    pdf->y_pos -= ST_SPACE;
+    write_to_pdf(pdf, sub_titel_buffer, (pdf->width - tw) / 2);
+    set_font_and_size(pdf, B_FONT, B_SIZE);
+    pdf->y_pos -= NL_SPACE;
+}
+
 void save_pdf(PDF* pdf, const char* file_name){
     HPDF_SaveToFile(pdf->doc, file_name);
 }
--- pdf.h
+++ pdf.h
@@ -1,5 +1,22 @@
+#pragma once
+#include <string.h>
+#include <stdio.h>
 #include <time.h>
 #include <hpdf.h>
+#include "get_time.h"
+
+#define TITLE "EVENT REPORT"
+#define T_FONT "Helvetica-Bold"
+#define T_SIZE 24
+#define SUB_TITLE "Generated on: "
+#define ST_FONT "Helvetica"
+#define ST_SIZE 18
+#define B_FONT "Helvetica"
+#define B_SIZE 12
+#define TIME_LEN 128
+#define BORDER 50
+#define ST_SPACE 30
+#define NL_SPACE 20
 
 typedef struct PDF{
     HPDF_Doc doc;
@@ -15,3 +32,4 @@ void set_font_and_size(PDF*, const char*, int);
 void write_to_pdf(PDF*, const char*, int);
 void save_pdf(PDF*, const char*);
 void free_pdf(PDF*);
+void init_pdf(PDF*);
