5f78731918f665699e52476a569220dd35ca0389
shlomoPearl <shlomoper94@gmail.com>
2026-02-04 23:50:38 +0200

    add time init and insert time value to struct tm

--- get_time.c
+++ get_time.c
@@ -1,5 +1,11 @@
 #include "get_time.h"
 
+void init_time(struct tm* timeinfo) {
+    time_t now;
+    time(&now);
+    localtime_r(&now, timeinfo);
+}
+
 void get_current_time_str(char* buffer, size_t size) { 
     time_t now;
     struct tm *local_time;    
@@ -8,6 +14,6 @@ void get_current_time_str(char* buffer, size_t size) {
     strftime(buffer, size, "%Y-%m-%d %H:%M:%S", local_time);
 }
 
-void get_current_time(time_t* t){
-    time(t);
+void get_time_from_struct(char* buffer, struct tm* timeinfo, size_t size) {
+    strftime(buffer, size, "%Y-%m-%d %H:%M:%S", timeinfo);
 }
--- get_time.h
+++ get_time.h
@@ -2,5 +2,6 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+void init_time(struct tm*);
 void get_current_time_str(char*, size_t);
-void get_current_time(time_t*);
+void get_time_from_struct(char*, struct tm*, size_t);
